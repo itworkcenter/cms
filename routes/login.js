abdce4f1f4de143703598697182a4a1d0abad4de
Carl <ly.live@163.com>
2015-07-04 02:15:42 +0800

    data connect success

--- routes/login.js
+++ routes/login.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var router = express.Router();
+<<<<<<< HEAD
 var submit = require("../dao/submit");
+=======
+var dao = require("../dao");
+>>>>>>> parent of 5c0cc0b... db change to localhost
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'SuperPort' ,data:"Null" });
+  res.render('login', { title: 'SuperPort' });
 });
 router.post('/verify', function(req, res, next) {
   var reqs=req.body;
 
+<<<<<<< HEAD
   if(reqs.user && reqs.pass){
       submit.post({
           path:"/rest/verify",
@@ -23,8 +28,12 @@ router.post('/verify', function(req, res, next) {
 
   }else{
       res.render('login', { title: 'SuperPort',data:"Null" });
+=======
+  if(user && pass){
+    console.log(dao)
+>>>>>>> parent of 5c0cc0b... db change to localhost
   }
-
+  res.render('login', { title: 'SuperPort' });
 });
 
 module.exports = router;
