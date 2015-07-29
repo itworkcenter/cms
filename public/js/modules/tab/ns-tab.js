052e83c13f09895deff8f9620f834d10bdb70340
Carl.Y.Liu <Carl.Y.Liu@newegg.com>
2015-07-29 17:20:55 +0800

    update tab ui code

--- public/js/modules/tab/ns-tab.js
+++ public/js/modules/tab/ns-tab.js
@@ -7,32 +7,34 @@
     'use strict';
 
     var Obj = function (opts) {
-        return new Tab.fn.init(opts);
+        return new Obj.fn.init(opts);
     };
+
     Obj.VERSION = "1.0";
-    Obj.fn = Tab.prototype;
+    Obj.TRANSITION_DURATION = 150;
+    Obj.DEFS={
+        handles: {
+            event: "click",
+            data: "",
+            selecter: ".ns-tab-item",
+            back: ""
+        },
+        container: ".ns-tab"
+    };
+
+    Obj.fn = Obj.prototype;
 
     /*Initialize*/
-    Obj.prototype.init = function (opts) {
+    Obj.fn.init = function (opts) {
 
-        var this_ = this;
+        var ths = this;
 
-        this.defs = {
-            handles: {
-                event: "click",
-                data: "",
-                selecter: ".ns-tab-item",
-                back: ""
-            },
-            container: ".ns-tab"
-        };
-        opts && this.ext(this.defs, opts);
-        opts && this.ext(this, this.defs);
+        opts && this.ext(this.DEFS, Obj.DEFS);
+        opts && this.ext(this.DEFS, opts);
 
-        var $tab = this.$tab = $(this.defs.container),
-            $hdls = this.handles = this.defs.handles;
 
-        console.log($tab)
+        var $el = this.$el = $(this.DEFS.container),
+            $hdls = this.handles = this.DEFS.handles;
 
         this.handle({
             event: $hdls.event,
@@ -44,10 +46,100 @@
         });
     };
 
+    Obj.fn.show = function () {
+        var $this    = this.container;
+        var $ul      = $this.closest('ul:not(.dropdown-menu)');
+        var selector = $this.data('target');
+
+        if (!selector) {
+            selector = $this.attr('href')
+            selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '') // strip for ie7
+        }
+
+        if ($this.parent('li').hasClass('active')) return;
+
+        var $previous = $ul.find('.active:last a');
+        var hideEvent = $.Event('hide.bs.tab', {
+            relatedTarget: $this[0]
+    })
+        var showEvent = $.Event('show.bs.tab', {
+            relatedTarget: $previous[0]
+        })
+
+        $previous.trigger(hideEvent)
+        $this.trigger(showEvent)
+
+        if (showEvent.isDefaultPrevented() || hideEvent.isDefaultPrevented()) return
+
+        var $target = $(selector)
+
+        this.activate($this.closest('li'), $ul)
+        this.activate($target, $target.parent(), function () {
+            $previous.trigger({
+                type: 'hidden.bs.tab',
+                relatedTarget: $this[0]
+            })
+            $this.trigger({
+                type: 'shown.bs.tab',
+                relatedTarget: $previous[0]
+            })
+        })
+    }
+
+    Obj.fn.activate = function (element, container, callback) {
+        var $active    = container.find('> .active')
+        var transition = callback
+            && $.support.transition
+            && ($active.length && $active.hasClass('fade') || !!container.find('> .fade').length)
+
+        function next() {
+            $active
+                .removeClass('active')
+                .find('> .dropdown-menu > .active')
+                .removeClass('active')
+                .end()
+                .find('[data-toggle="tab"]')
+                .attr('aria-expanded', false)
+
+            element
+                .addClass('active')
+                .find('[data-toggle="tab"]')
+                .attr('aria-expanded', true)
+
+            if (transition) {
+                element[0].offsetWidth // reflow for transition
+                element.addClass('in')
+            } else {
+                element.removeClass('fade')
+            }
+
+            if (element.parent('.dropdown-menu').length) {
+                element
+                    .closest('li.dropdown')
+                    .addClass('active')
+                    .end()
+                    .find('[data-toggle="tab"]')
+                    .attr('aria-expanded', true)
+            }
+
+            callback && callback()
+        }
+
+        $active.length && transition ?
+            $active
+                .one('bsTransitionEnd', next)
+                .emulateTransitionEnd(Tab.TRANSITION_DURATION) :
+            next()
+
+        $active.removeClass('in')
+    }
+
+
+
     /*Event*/
-    Obj.prototype.handle = function (json) {
-        var this_ = this;
-        $("body").on(
+    Obj.fn.handle = function (json) {
+        var ths = this;
+        $(document).on(
             json.event,
             json.selecter, /*ns-menu-a*/
             json.data,
@@ -56,7 +148,7 @@
                 console.log(this)
 
 
-                json.back.apply(this_);
+                json.back.apply(ths);
                 e.stopPropagation();
                 return false;
             }
@@ -71,8 +163,8 @@
             $(".ns-menu-open").removeClass("ns-menu-open");
         }
     };
-    Obj.prototype.handlefuntrans = function () {
-        var this_ = this,
+    Obj.fn.handlefuntrans = function () {
+        var ths = this,
             $item = this.$item,
             $a = $item.children(".ns-menu-a"),
             $main = $item.children(".ns-menu-main"),
@@ -89,8 +181,8 @@
         }
 
     };
-    Obj.prototype.handlefun = function () {
-        var this_ = this,
+    Obj.fn.handlefun = function () {
+        var ths = this,
             $item = this.$item,
             $a = $item.children(".ns-menu-a"),
             $main = $item.children(".ns-menu-main"),
@@ -107,8 +199,8 @@
         }
         /*check direction*/
         if (isItem) {
-            var itemProps = this_.props($item),
-                mainProps = this_.props($main);
+            var itemProps = ths.props($item),
+                mainProps = ths.props($main);
             //console.log(mainProps.max + " " + (mainProps.left + mainProps.width));
             //console.log(mainProps.max - (mainProps.left + mainProps.width));
 
@@ -138,18 +230,18 @@
 
     };
     /*Get Property*/
-    Obj.prototype.props = function ($obj) {
+    Obj.fn.props = function ($el) {
         var rslts = {};
-        this.ext(rslts, $obj.offset());
-        rslts.width = $obj.width();
-        rslts.height = $obj.height();
+        this.ext(rslts, $el.offset());
+        rslts.width = $el.width();
+        rslts.height = $el.height();
         rslts.right = rslts.left + rslts.width;
         rslts.bottom = rslts.top + rslts.height;
         rslts.max = $(window).width();
         return rslts;
     };
     /*Extend Object*/
-    Obj.prototype.ext = function (o, s) {
+    Obj.fn.ext = function (o, s) {
         if (o && s) {
             for (var i in s) {
                 if (typeof s[i] === "object") {
@@ -170,6 +262,50 @@
         return o;
     };
     Obj.fn.init.prototype = Obj.fn;
+
+
+
+    // TAB PLUGIN DEFINITION
+    // =====================
+
+    function Plugin(option) {
+        return this.each(function () {
+            var $this = $(this)
+            var data  = $this.data('bs.tab')
+
+            if (!data) $this.data('bs.tab', (data = new Tab(this)))
+            if (typeof option == 'string') data[option]()
+        })
+    }
+
+    var old = $.fn.tab
+
+    $.fn.tab             = Plugin
+    $.fn.tab.Constructor = Obj
+
+
+    // TAB NO CONFLICT
+    // ===============
+
+    $.fn.tab.noConflict = function () {
+        $.fn.tab = old
+        return this
+    }
+
+
+    // TAB DATA-API
+    // ============
+
+    var clickHandler = function (e) {
+        e.preventDefault()
+        Plugin.call($(this), 'show')
+    }
+
+    $(document)
+        .on('click.bs.tab.data-api', '[data-toggle="tab"]', clickHandler)
+        .on('click.bs.tab.data-api', '[data-toggle="pill"]', clickHandler)
+
+
 
     if (typeof define === 'function' && define.amd) {
         // AMD (Register as an anonymous module)
